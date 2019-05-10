7a3bace02638c9cf76771c5fb23a5677d59e349f
mqasemi <m_qasemi6974@yahoo.com>
2019-05-10 18:36:50 +0430

    Update Main.java
    server change commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-         System.out.println("Hello World!");
-         System.out.println("Hello World!2"); 
-        System.out.println("Hello World!3");
-         System.out.println("Hello World!4");
-         System.out.println("Hello World!5");
-
-
-
-
+         System.out.print("this is server change");
     }
 }
