af31d72c96822d1981c937873aae8ee7b69e503f
mqasemi <m_qasemi6974@yahoo.com>
2019-05-10 18:15:16 +0430

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("test");
-        
-         System.out.println("test3");
-        
-        
-         System.out.println("test2");
+         System.out.println("Hello World!2"); System.out.println("Hello World!3");
+         System.out.println("Hello World!4");
+         System.out.println("Hello World!5");
     }
 }
